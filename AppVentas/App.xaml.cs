b7a6c89e4e7ed7410cecc02756558ba7e5f1767e
carrion2798 <carrionjohan2798@gmail.com>
2021-08-21 10:59:53 -0600

    updated link

--- AppVentas/App.xaml.cs
+++ AppVentas/App.xaml.cs
@@ -11,7 +11,7 @@ namespace AppVentas
     {
         public static string cedula = string.Empty;//Se actualiza cuando se hace login
         public static string correo = string.Empty;
-        public static string url = "https://a945482ed86d.ngrok.io/api/";//se debe cambiar
+        public static string url = "https://0d15-201-191-51-28.ngrok.io/api/";//se debe cambiar
         public static IList<ArticuloModel> Carrito = new ObservableCollection<ArticuloModel>();//Variable estatica de carrito
         public App()
         {
